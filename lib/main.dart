f92b7e308c108d7f8c9dc21bf6d986e67c539e18
julien <juliensavary2001@gmail.com>
2023-11-17 16:20:26 -0800

    build files maybe an unecessary commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
+// FIREBASE
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
